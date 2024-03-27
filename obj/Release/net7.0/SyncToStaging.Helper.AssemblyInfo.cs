0fc5d963c34674dfdf1b5b0300fca44441ab120c
DESKTOP-1D9RIE2\admin <tungphien.cn08@gmail.com>
2024-03-27 14:05:37 +0700

    version 1.1.3

--- obj/Release/net7.0/SyncToStaging.Helper.AssemblyInfo.cs
+++ obj/Release/net7.0/SyncToStaging.Helper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyncToStaging.Helper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.3.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.3+7cefa2bc95d2fcf759dd9977684f13c8593ed334")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyncToStaging.Helper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyncToStaging.Helper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.3.0")]
